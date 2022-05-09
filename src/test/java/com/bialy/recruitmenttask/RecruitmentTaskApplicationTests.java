8b4aafb0a9060a9f779af083d43cff258e5fd5c4
B1AL1 <k.konrad9902@gmail.com>
2022-05-09 22:42:42 +0200

    Initial commit

---
+++ src/test/java/com/bialy/recruitmenttask/RecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bialy.recruitmenttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
